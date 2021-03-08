cdc3a5ca59e06042bb1cc4992481eb4d52394990
SneyderHOL <1507@holbertonschool.com>
2021-03-08 18:35:47 -0500

    Add comments to regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  VALID_USERNAME_REGEX = /\A[\w]+\z/
+  # VALID_USERNAME_REGEX = /\A[\w]+\z/
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
-  VALID_FULLNAME_REGEX = /\A[a-zA-Z ]+\z/
+  # VALID_FULLNAME_REGEX = /\A[a-zA-Z ]+\z/
   validates :fullname, presence: true,
             uniqueness: { case_sensitive: false },
             length: { maximum: 140 }
